b9457117e4bb101da2e897928e2dc3f99d747d3b
Linda Adzigbli <106244819+Norkplim22@users.noreply.github.com>
2023-05-14 22:28:21 +0200

    eda

--- eda.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-df_test = pd.read_csv('D:\LP6\Embedding-ML-Model-into-a-Web-App\dataset\Paitients_Files_Test.csv')
-df_train = pd.read_csv('D:\LP6\Embedding-ML-Model-into-a-Web-App\dataset\Paitients_Files_Train.csv')
-
-df_test.shape, df_train.shape
-df_train
